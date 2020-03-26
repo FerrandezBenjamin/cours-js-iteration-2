ec1cbde63e84f490006d5b4461901931258237c6
FerrandezBenjamin <58741067+FerrandezBenjamin@users.noreply.github.com>
2020-03-26 16:53:51 +0100

    upDateAll

--- public/static/js/objects.js
+++ public/static/js/objects.js
@@ -5,10 +5,30 @@
  *    - charger la liste des objets depuis l'API
  *    - charger les données des objets dans la table
  */
-function load_components(){
-    console.log("Chargement des données de la page");
-    // Ajouter ici le code permettant de charger dynamiquement les éléments de la page
+function load_components()
+{
+   $("button").click(function()
+    {
+        $.get("/data", function(data, status){
+          
+          console.log(data);
+        });
+    });
+      console.log("Chargement des données de la page");
+        // Ajouter ici le code permettant de charger dynamiquement les éléments de la page     
 }
 
-load_components();
 
+function add_line_table(){
+
+
+    let line = '<th style="width: 100px">xx</th>\
+    <th>xx</th>\
+    <th>xx</th>\
+    <th style="width: 100px"><input type ="checkbox"></th>\
+    <th style="width: 100px"><button class="btn-primary">xx</button></th>'
+
+    // $('#table_body').append(line); Façon Jquery !
+    document.getElementById('table_body').innerHTML += line;
+
+}
