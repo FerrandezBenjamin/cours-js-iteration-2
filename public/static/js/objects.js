20da9f2e2684b2af09375d600612da3bf64b11c7
FerrandezBenjamin <58741067+FerrandezBenjamin@users.noreply.github.com>
2020-03-27 15:25:07 +0100

    Les produits sont maintenant affiché lorsque je clique sur le bouton

--- public/static/js/objects.js
+++ public/static/js/objects.js
@@ -5,30 +5,33 @@
  *    - charger la liste des objets depuis l'API
  *    - charger les données des objets dans la table
  */
-function load_components()
-{
-   $("button").click(function()
-    {
-        $.get("/data", function(data, status){
-          
-          console.log(data);
-        });
-    });
-      console.log("Chargement des données de la page");
-        // Ajouter ici le code permettant de charger dynamiquement les éléments de la page     
+function load_components() {
+  $.get("/objects", function (data) {
+    for (let object of data.objects) {
+      add_line_table(object);
+    }
+  });
 }
 
+function add_line_table(data) {
+  
+  let checked ="";
 
-function add_line_table(){
+    if (data.status){
+      checked = "checked";
+    } else{
+       checked = "";
+    }
 
+  let line = ' <tr>\
+  <th style="width: 100px">'+data.serial+'</th>\
+    <th><img style="max-width:30%; heigth:10%;" src=" static/images/'+data.image+'"></th>\
+    <th>'+data.description+'</th>\
+    <th style="width: 100px"><input type ="checkbox" '+checked+'></th>\
+    <th style="width: 100px"><button class="btn-primary"></button></th>\
+    </tr>'
 
-    let line = '<th style="width: 100px">xx</th>\
-    <th>xx</th>\
-    <th>xx</th>\
-    <th style="width: 100px"><input type ="checkbox"></th>\
-    <th style="width: 100px"><button class="btn-primary">xx</button></th>'
+  // $('#table_body').append(line); Façon Jquery !
 
-    // $('#table_body').append(line); Façon Jquery !
-    document.getElementById('table_body').innerHTML += line;
-
+  document.getElementById('table_body').innerHTML += line;
 }
